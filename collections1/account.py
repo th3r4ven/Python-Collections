40ca59aa3b2051eaa478d282208b6a6cd4cdd991
Matheus Chiarato <matheus.chiarato@outlook.com>
2021-01-20 15:55:30 -0300

    Classes with tuple and list functions with Dunder methods

--- collections1/account.py
+++ collections1/account.py
@@ -1,29 +1,71 @@
+from abc import ABCMeta, abstractmethod
+
+
 class Account:
 
+    __metaClass__ = ABCMeta
+
     def __init__(self, code):
-        self.__code = code
-        self.__money = 0
+        self._code = code
+        self._money = 0
+
+    @property
+    def code(self):
+        return self._code
 
     def deposit(self, value):
-        self.__money += value
+        self._money += value
+
+    def __lt__(self, other):
+        return self._money < other._money
+
+    def __eq__(self, other):
+        return isinstance(other, Account) and self._code == other._code
 
     def __str__(self):
-        return f'Bank code: {self.__code} Money: {self.__money}'
+        return f'Bank code: {self._code} Money: {self._money}'
+
+    @abstractmethod
+    def tax_per_month(self):
+        pass
 
 
-def deposit_everyone(accounts):
-    for account in accounts:
-        account.deposit(100)
+class Normal_account(Account):
+
+    def tax_per_month(self):
+        self._money -= 2
+
+
+class Saving_account(Account):
+
+    def tax_per_month(self):
+        self._money *= 1.01
+        self._money -= 3
 
 
 if __name__ == '__main__':
 
     account01 = Account(666)
+    account01 = Saving_account(account01.code)
     account02 = Account(555)
+    account02 = Normal_account(account02.code)
     account01.deposit(100)
     account02.deposit(500)
-
     accounts = (account01,  account02)
-    deposit_everyone(accounts)
 
-    print(accounts)
+    for account in accounts:
+        account.tax_per_month()  # duck typing
+
+    print(accounts[0])
+
+    account01 = Account(666)
+    account02 = Account(666)
+    account02 = Saving_account(account02.code)
+
+    if account01 == account02:
+        print("True")
+
+    # Sorted function, using account.money as key param to sort
+
+    for account in sorted(accounts):
+        print(account)
